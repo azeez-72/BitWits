6104881fafcb930de0f5287ff14f8fdbfabd8df1
azeez-72 <officialazeezamd@gmail.com>
2020-05-24 08:11:48 +0530

    check

---
+++ BitWits/lib/New_Class.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class New_Class extends StatefulWidget {
+  static final String id = 'new_class';
+
+  @override
+  _New_ClassState createState() => _New_ClassState();
+}
+
+class _New_ClassState extends State<New_Class> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
