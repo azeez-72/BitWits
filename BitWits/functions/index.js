ebdfb0f5c0959b6648e323a57864e80e979a38e5
Sahil Kedare <sahilkedare4444@gmail.com>
2020-06-10 19:32:58 +0530

    cloud fn added

---
+++ BitWits/functions/index.js
@@ -0,0 +1,15 @@
+const functions = require('firebase-functions');
+
+exports.myFunction = functions.firestore
+  .document('bitwits/{notification}')
+  .onCreate((change, context) => {
+     console.log(change.after.data());
+   });
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
