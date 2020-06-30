a6d72d35b27270517548c7517b01ec82772756ae
Pankaj Khushalani <53428217+pk-218@users.noreply.github.com>
2020-07-01 00:19:40 +0530

    Removed CircularCheckBox import

--- BitWits/lib/Main_Screen/Assignments/Assignments.dart
+++ BitWits/lib/Main_Screen/Assignments/Assignments.dart
@@ -6,7 +6,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:bitwitsapp/Utilities/constants.dart';
-import 'package:circular_check_box/circular_check_box.dart';
+//import 'package:circular_check_box/circular_check_box.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'add_assignment.dart';
